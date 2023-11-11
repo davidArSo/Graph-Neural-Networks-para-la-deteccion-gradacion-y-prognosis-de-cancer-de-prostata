8f3009199735fc28cf1896b9e4778320844d2b76
David Armenteros Soto <86359972+davidArSo@users.noreply.github.com>
2023-11-11 18:56:53 +0100

    Upload test_model.py

--- test_model.py
+++ test_model.py
@@ -2,19 +2,20 @@
 test_model.py - Funciones para la evaluación del modelo GNN.
 
 Este fichero proporciona funciones de evaluación del modelo GNN en tareas 
-de clasificación de grados gleason, isup y detección de cáncer.
+de clasificación de grados gleason (primario, secundario y ambos), isup y detección de cáncer.
 
 Contenido:
   - test_gleason(model, device, test_loader, criterion, gleason_mapping, test_primary=True, test_secondary=True)
+  - test_sum_gleason(model, device, test_loader, criterion, gleason_mapping)
   - test_isup(model, device, test_loader, criterion)
   - test_cancer_present(model, device, test_loader, criterion)
 
 Uso:
-  from test_model import test_gleason, test_isup, test_cancer_present
+  from test_model import test_gleason, test_sum_gleason, test_isup, test_cancer_present
 """
 
 import torch
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_recall_fscore_support, cohen_kappa_score
 
 def test_gleason(model, device, test_loader, criterion, gleason_mapping, test_primary=True, test_secondary=True):
   model.to(device)
@@ -77,8 +78,9 @@ def test_gleason(model, device, test_loader, criterion, gleason_mapping, test_pr
       combined_accuracy = correct_combined / total
 
       precision, recall, f1, _ = precision_recall_fscore_support(combined_labels, combined_predicted, average='weighted', zero_division=1)
+      kappa = cohen_kappa_score(combined_labels, combined_predicted)
 
-      return avg_loss, combined_accuracy, precision, recall, f1
+      return avg_loss, combined_accuracy, precision, recall, f1, kappa
 
     else:
       if test_primary:
@@ -89,9 +91,46 @@ def test_gleason(model, device, test_loader, criterion, gleason_mapping, test_pr
           predicted_labels = predicted_secondary_labels
 
       precision, recall, f1, _ = precision_recall_fscore_support(true_labels, predicted_labels, average='weighted', zero_division=1)
+      kappa = cohen_kappa_score(true_labels, predicted_labels)
 
-      return avg_loss, primary_accuracy or secondary_accuracy, precision, recall, f1
+      return avg_loss, primary_accuracy or secondary_accuracy, precision, recall, f1, kappa
 
+def test_sum_gleason(model, device, test_loader, criterion, gleason_mapping):
+  model.to(device)
+  model.eval()
+
+  running_loss = 0.0
+  correct = 0
+  total = 0
+  predicted_labels = []
+  true_labels = []
+
+  with torch.no_grad():
+      for batch_idx, graph in enumerate(test_loader):
+        graph = graph.to(device)
+
+        gleason_output = model(graph)
+
+        gleason_labels = [primary.item() + secondary.item() for primary, secondary in zip(graph.gleason_primary, graph.gleason_secondary)]
+        gleason_target = torch.tensor([gleason_mapping[label] for label in gleason_labels], device=device)
+
+        loss = criterion(gleason_output, gleason_target)
+        running_loss += loss.item()
+
+        _, predicted = torch.max(gleason_output.data, 1)
+        correct += (predicted == gleason_target).sum().item()
+        predicted_labels.extend(predicted.cpu().numpy())
+        true_labels.extend(gleason_target.cpu().numpy())
+
+        total += gleason_target.size(0)
+
+  avg_loss = running_loss / len(test_loader)
+  accuracy = correct / total
+  precision, recall, f1, _ = precision_recall_fscore_support(true_labels, predicted_labels, average='weighted', zero_division=1)
+  kappa = cohen_kappa_score(true_labels, predicted_labels)
+
+  return avg_loss, accuracy, precision, recall, f1, kappa
+  
 def test_isup(model, device, test_loader, criterion):
   model.to(device)
   model.eval()
@@ -125,8 +164,9 @@ def test_isup(model, device, test_loader, criterion):
     avg_loss = running_loss / len(test_loader)
 
     precision, recall, f1, _ = precision_recall_fscore_support(true_labels, predicted_labels, average='weighted', zero_division=1)
+    kappa = cohen_kappa_score(true_labels, predicted_labels)
 
-    return avg_loss, accuracy, precision, recall, f1
+    return avg_loss, accuracy, precision, recall, f1, kappa
     
 def test_cancer_present(model, device, test_loader, criterion):
   model.to(device)
@@ -161,5 +201,6 @@ def test_cancer_present(model, device, test_loader, criterion):
     avg_loss = running_loss / len(test_loader)
 
     precision, recall, f1, _ = precision_recall_fscore_support(true_labels, predicted_labels, average='binary', zero_division=1)
+    kappa = cohen_kappa_score(true_labels, predicted_labels)
 
-    return avg_loss, accuracy, precision, recall, f1
+    return avg_loss, accuracy, precision, recall, f1, kappa
